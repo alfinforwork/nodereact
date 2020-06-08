8f13be54f192f44dac00c39f455a475b7690f25c
Mohamad Alfin Nahrowi <alfinforwork@gmail.com>
2020-06-08 11:20:55 +0700

    s

--- backend/models/Login.js
+++
@@ -1,7 +0,0 @@
-exports.getAll = (con, email, callback) => {
-  con.query(
-    // "select * from sekolah where email_sekolah='" + email + "'",
-    "select * from sekolah where email_sekolah='" + email + "'",
-    callback
-  );
-};
